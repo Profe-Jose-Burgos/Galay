b6ef6af626825be10ecd03ac9e3cbf5c5832cff1
Kuku014 <91647034+Kuku014@users.noreply.github.com>
2023-01-23 16:59:17 -0500

    cambiosso
    cambioso

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ integrantes
 *Jair Alberto Petana Mina
 *Jesús Roberto aldeano castro
 *Ryan Roberto Charles Echeverría
+
+Paso 1:
+primero se entrena el chatbot con este comando
+            "pip setup.py install"
+paso 2:
+entrar al entorno de python y escribir esto:
+        from chatbot import demo
+luego le das enter y escribir esto
+        demo()
---
+++ build/lib/chatbot/constants.py
@@ -0,0 +1,16 @@
+FIRST_QUESTIONS = {
+    "en": "Hi, how are you?",
+    "de": "Hallo, wie geht es dir?",
+    "pt-br": "Oi como você está?",
+    "he": "היי מה קורה?",
+    "es": "¡Hola! Soy un chatbot,tu asistente virtual.\n Por favor, escoge una opción escribiendo el número para ayudarte con tu consulta. \n¿Cómo puedo ayudarte?\n 1.Banca en línea \n2.Cuentas \n3.Tarjetas \n4.Créditos \n5.Seguros \n6.Servicios \n7.Canales de atención \n8.Cerrar sesión",
+}
+TERMINATES = {
+    "en": "quit",
+    "de": "ende",
+    "pt-br": "sair",
+    "he": "זהו",
+   "es": "salir"and "8"
+}
+
+LANGUAGE_SUPPORT = ["en", "de", "pt-br", "he","es"]
